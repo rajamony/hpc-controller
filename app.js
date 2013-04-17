05c112c3bf555df1b5d0c8a3ca7239da26d4f366
Ram Rajamony <rajamony@us.ibm.com>
2013-04-17 09:25:29 -0500

    still trying to get login and sessions to work

--- app.js
+++ app.js
@@ -1,147 +1,168 @@
 /*
  * Simple server for HPC Controller
  * Author: Ram Rajamony (rajamony@us.ibm.com)
- *
- * Main server-side file, processed by node.js
 **/
-
 "use strict";
 
+var port 	= process.env.PORT || 9080;
+var hostname 	= process.env.HOSTURL || "0.0.0.0";
+var SITE_SECRET = Math.random().toString();
+
 var 	fs = require ('fs'),
-	http = require ('http'),
 	crypto = require ('crypto'),
 	assert = require ('assert'),
+	connect = require ('./node_modules/express/node_modules/connect'),
+	cookie = require('cookie'),
 	express  = require ('express'),
-	mongodb = require ('mongodb'),
-	utils = require ('js/utils.js'),
-	MongoUsers = null,
-	MongoAudio = null,
+	mongoose = require ('mongoose'),
+	utils = require ('./js/utils.js'),
+	// users = require('./js/users.js'),
+	NO_MORE_REQUIRES;
 
-	port           = process.env.PORT || 8090,
-	hostname	= process.env.HOSTURL || "0.0.0.0",
+var	io = null,
+	sessionstore_options = {
+		auto_reconnect: true,
+		url: 'mongodb://localhost:27017/hpc/sessions',
+		stringify: true,
+		// clear_interval: -1,
+	    },
+	sessionstore = new require('connect-mongo')(express)(sessionstore_options),
+	sioCookieParser = express.cookieParser(SITE_SECRET),
+	models = {};
 
-	tts_options = {
-	    method: 'POST',
-	    path: '/TTS/',
-	    port: 80,
-	    hostname: 'texttospeech.ibmeos.com',
-	    // port: 1337,
-	    // hostname: 'localhost',
-	    headers: {"content-type" : "application/json; charset=UTF-8"},
-	},
-
-	NO_MORE_VARS; // Thats all folks
-
-
-
-function HandleStaticFile (req, res) {
-    console.log ("Got a HandleStaticFile for <" + req.url + ">");
-    res.sendfile ("." + req.url);
+function HandleError (err, req, res, next) {
+    console.log ("\n\nInternal error?????\n");
+    res.status (500); 
+    res.end (JSON.stringify({error: err}));
 }
 
-
-/*
- * Opens connection to MongoDB, sets up hooks for processing different requests and starts listening. 
- * All other processing happens through callbacks
-**/
 function Initialize () {
-    mongodb.MongoClient.connect ('mongodb://localhost:27017/hpcusers', {auto_reconnect: true},
-        function (err, db) {
-	    assert.equal (err, null, "Could not open connection to MongoDB. Error <" + String(err) + ">");
+    var db = mongoose.connect ('mongodb://localhost/hpc');
+    var app = express();
+    var ssl_options = { key: fs.readFileSync('/opt/keys/root-ca.key'), cert: fs.readFileSync('/opt/keys/cert.pem')};
+//    var server = require('https').createServer (ssl_options, app);
+    var server = require('http').createServer (app);
+    models.users = require ('./mongoose-models/users')(mongoose).model;
+    io = require('socket.io').listen(server);
 
-	    var server = express();
-	    server.use (express.bodyParser());		// To handle POSTs
-	    server.get('/*', HandleStaticFile);	// Anything but the above gets treated as a static file
-	    server.post ('/Perform', HandlePosts);
-	    db.createCollection ('users', function (err, collection) {
-		assert.equal (err, null, "Could not open/create collection <users> within MongoDB. Error <" + String(err) + ">");
-		MongoUsers = collection;
+    app.set ('case sensitive routing', true);
+    app.use (app.router);			// Dynamic routes come first, then static files. See http://tinyurl.com/afab75h
+    app.use (express.logger('dev'));
+    app.use (express.bodyParser());		// To handle POSTs.
+    app.use(express.methodOverride());
+    app.use(express.cookieParser()); // app.use(express.cookieParser(SITE_SECRET));
+    app.use(express.session({secret: SITE_SECRET, cookie: {maxAge: 10*1000}, store: sessionstore}));
+// http://tinyurl.com/cbr8u9c
 
-		server.listen (port, hostname, function () {
-			var myroot = "http://" + hostname + ":" + port;	// FIXME: What if we're https?
-			console.log ( "Server is now advertising as " + myroot);
+app.use(function(req, res){
+  var body = '';
+  if (req.session.views) {
+    ++req.session.views;
+  } else {
+    req.session.views = 1;
+    body += '<p>First time visiting? view this page in several browsers :)</p>';
+  }
+  res.send(body + '<p>viewed <strong>' + req.session.views + ' session id ' + req.sessionID + ' </strong> times.</p>');
+});
+
+    app.use ('/html', express.static(__dirname + '/html', {maxAge: 0}));	// Stale it immediately during development
+    app.use ('/css', express.static(__dirname + '/css', {maxAge: 0}));	// Stale it immediately during development
+    app.use ('/js', express.static(__dirname + '/js', {maxAge: 0}));	// Stale it immediately during development
+
+    // app.all ('*', AuthenticateUser, LoadUser);
+    app.use (HandleError); // app.use (express.errorHandler());
+
+    server.listen (port, hostname, function () { 
+	console.log ( "Server is now advertising itself as https://" + hostname + ":" + port); 
+	// SetupSocketIO();
+    });
+}
+
+function SetupSocketIO () {
+    io.set ('log level', 1);
+    io.sockets.on ('connection', function (socket) {
+// console.log (sioCookieParser.toString());
+        sioCookieParser (socket.handshake, {}, function(err) {
+// console.dir (socket.handshake);
+		sessionstore.get (socket.handshake.cookies["express.sid"], function(err, sessionData) {
+			console.log ("SESSIONSTORE error = <" + err + "> sessionData = <" + sessionData + ">");
+			// session data available here
 		    });
 	    });
-	});
-}
 
+	for (var i = 0; i < 10; i++)
+	    socket.emit ('news', { hello: 'world ' + i });
+	socket.on ('my other event', function (data) {
+		console.log(data);
+	    });
+    });
 
-function HandlePosts (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/javascript;charset=UTF-8'});	// We always respond with a JSON
-    var jsondata = new utils.RadioFromBrowser(req.body);
-    if (jsondata.ValidateOrRespondWithError (res)) {
-	switch (jsondata.action) {
-	    case "doesuserexist": DoesUserAlreadyExist (req, res, jsondata); break;
-	    case "signup": AddNewUser (req, res, jsondata); break;
-	    case "signin": LoginExistingUser (req, res, jsondata); break;
+/*
+    io.set ('authorization', function (handshakeData, callback) {
+//	    // Check database, ensure the user has already logged in.
+//	    // If not, direct them to a login screen
+
+    var accept = callback;
+    if (handshakeData.headers.cookie) {
+	handshakeData.cookie = cookie.parse(decodeURIComponent(handshakeData.headers.cookie));
+	handshakeData.sessionID = connect.utils.parseSignedCookie (handshakeData.cookie['express.sid'], SITE_SECRET);
+	console.log ("Got cookie <" + handshakeData.cookie['express.sid'] + "> and sessionID is <" + handshakeData.sessionID + ">");
+	if (handshakeData.cookie['express.sid'] == handshakeData.sessionID) {
+	    console.log ("    but it is an invalid cookie");
+	    return accept('Cookie is invalid.', false);
 	}
+    } else {
+	console.log ("No cookie was sent");
+	return accept('No cookie transmitted.', false);
     }
+    accept(null, true);
+});
+*/
+
 }
 
+/*
+var sessionobj = {}; //This is important; it will contain your connect.sid IDs.
 
-function DoesUserAlreadyExist (req, res) {
-    UserModel.findOne ({_id: req.body.username}, function (err, doc) {
-	    var jsonresp = { error: (err === null) ? "" : "DoesUserAlreadyExist" + String(err), username: req.body.username, credentials: "" };
-	    if (doc === null)
-		jsonresp.credentials = "nosuchuser";
-	    else {
-		if (doc._id !== req.body.username)	// Why would Mongo give us a non-null document with a DIFFERENT _id?
-		    jsonresp.error += "DoesUserAlreadyExist: Something screwed up in Mongo";
-		else
-		    jsonresp.credentials = "useralreadyexists";
-	    }
-	    Debuglog ("DoesUserAlreadyExist: username <" + req.body.username + "> = " + jsonresp.credentials + ". Error = <" + ">");
-	    res.end (JSON.stringify(jsonresp));
-	});
-}
+app.use(express.session({ 
+    secret: 'secret_pw',
+    store: sessionStore,
+    cookie: { 
+        secure: true,
+        expires: new Date(Date.now() + 60 * 1000), //setting cookie to not expire on session end
+        maxAge: 60 * 1000,
+        key: 'connect.sid'
+    }
+}));
 
-/* Responds eventually with a JSON 
- * 	error:	      ""     If no errors were encountered. Remaining fields invalid if this is non-null
- *	username:     "..."  We echo back the supplied username
- *	credentials:  "..."  If user exists: "useralreadyexists". Otherwise, if user successfully added: "valid". Else ""
- * 	accesskey:    "..."  If the user was successfully added. This needs to be given to the Chrome plugin for all subsequent use
-**/
-function AddNewUser (req, res) {
-    var record = new UserModel();
-    record._id = req.body.username;
-    record.accesskey = record.password = req.body.password; // For now, we just send the cleartext password back
-    record.usersince = new Date();
-    // record.stations = [{ _id: "Adventure"}, {_id: "Kids"}];
-    record.save (function (err) {
-	    var jsonresp = { error: "", username: req.body.username, accesskey: "", credentials: "" };
-	    if (err === null) {
-		jsonresp.accesskey = record.accesskey;
-		jsonresp.credentials = "valid";
-	    }
-	    else {
-		if (-1 !== String(err).search("duplicate key error index"))
-		    jsonresp.credentials = "useralreadyexists"; 
-		else
-		    jsonresp.error = "AddNewUser: Error during saving of record: " + String(err);
-	    }
-	    Debuglog ("AddNewUser: Username <" + req.body.username + ">, credentials = " + jsonresp.credentials + ". Errors: <" + jsonresp.error + ">");
-	    res.end (JSON.stringify (jsonresp));
-	});
-}
+app.get("/*", function(req, res, next){
+    if(sessionobj[req.cookies['connect.sid']]){
+        if(sessionobj[req.cookies['connect.sid']].login = true{
+            //Authenticated AND Logged in
+        }
+        else{
+            //authenticated but not logged in
+        }
+    }
+    else{
+        //not authenticated
+    }
+});
 
-/* Responds eventually with a JSON
- * 	error:	     ""      If no errors were encountered. Remaining fields invalid if this is non-null
- *	username:    "..."   We echo back the supplied username
- *      credentials: "..."   Set to "valid" if the supplied password matches what we have in Mongo, set to "invalid" otherwise
- *	accesskey:   "..."   A secret we give back to the user so the plugin can tag its requests; same as password for now
-**/
-function LoginExistingUser (req, res) {
-    UserModel.findOne ({_id: req.body.username}, function (err, doc) {
-	    var jsonresp = { error: (err === null) ? "" : "LoginExistingUser: " + String(err), username: req.body.username, accesskey: "", credentials : "invalid" };
-	    if (err === null && doc !== null && doc.password === req.body.password) {
-	        jsonresp.credentials = "valid";
-		jsonresp.accesskey = doc.accesskey;
-	    }
-	    Debuglog ("LoginExistingUser: username <" + jsonresp.username + "> outcome " + jsonresp.credentials + ", accesskey <" + jsonresp.accesskey + "> with error: " + jsonresp.error);
-	    res.end (JSON.stringify (jsonresp));
-	});
-}
+io.sockets.on('connection', function(socket){
+    sessionobj[cookie.parse(socket.handshake.headers.cookie).'connect.sid'].login = false;
+    sessionobj[cookie.parse(socket.handshake.headers.cookie).'connect.sid'].socketid = socket.id;
+    socket.on('login', function(data){
+        //DB Call, where you authenticate login
+        //on callback (if login is successful):
+        sessionobj[cookie.parse(socket.handshake.headers.cookie).connect.sid] = true;
+    });
+    socket.on('disconnect', function(data){
+        //any cleanup actions you may want
+    });
+});
 
+*/
 
 
 Initialize();
