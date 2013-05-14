39365fb8d6ef53010a03d5c18c6e04e741d1cf0f
Ram Rajamony <rajamony@us.ibm.com>
2013-05-14 15:06:56 -0500

    Moved files, added startup code

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+##
+## load the system's configuration
+##
+. ./system.config
+
+##
+## Setup up SSL (https access)
+##
+if [ ! -d $SSLKEYDIR ]; then
+    echo "You need to manually create /opt/keys and make it r/w by the user who will run the controller"
+    exit 1
+fi
+
+if [ ! -e $SSLKEYDIR/cert.pem || ! -e $SSLKEYDIR/root-ca.key ]; then
+    echo "Setting up SSL certificates for https access"
+    rm -f $SSLKEYDIR/cert.pem $SSLKEYDIR/root-ca.key
+    openssl genrsa -out $SSLKEYDIR/root-ca.key
+    openssl req -new -key $SSLKEYDIR/root-ca.key -out /tmp/csr.pem.$$ -subj \
+    		"/C=$ssl_country/ST=$ssl_state/L=$ssl_locality/O=$ssl_organization/OU=$ssl_orgunit/CN=$ssl_name/emailAddress=$ssl_email"
+    openssl x509 -req -days 9999 -in /tmp/csr.pem.$$ -signkey $SSLKEYDIR/root-ca.key -out $SSLKEYDIR/cert.pem
+    rm -f /tmp/csr.pem.$$
+fi
+
+##
+## Setup Mongo and start it
+##
+mkdir -p $MONGODATABASEDIR	# In case this is the first time ever we are running this 
+rm -f $MONGOOUTFILE
+
+mongod --smallfiles --dbpath $MONGODATABASEDIR --quiet >& $MONGOOUTFILE &
+
+
+##
+## Now run the HPC controller web server in a loop
+## During development, the webserver code will be updated. A server-side hook
+## has been set up (on the server repo) that will do a git pull of the code and 
+## then request the webserver to exit (via an HTTP GET to /exitnow)
+
+while true; do
+    (cd $WEBSERVERDIR; node $WEBSERVER)
+done
