ab0c8bfd8975b00035ff35ad04272474a1e79ecb
Ram Rajamony <rajamony@us.ibm.com>
2013-05-14 11:00:32 -0500

    added copyright

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 /*
+ * Copyright 2013- IBM
+ * All rights reserved
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * Simple server for HPC Controller
- * Author: Ram Rajamony (rajamony@us.ibm.com)
-**/
-"use strict";
+ * Written by: Ram Rajamony, IBM Research, Austin, TX
+ *
+ */
+
+'use strict';
 
 var 	port 	= process.env.PORT || 9080,
 	hostname 	= process.env.HOSTURL || "0.0.0.0",
